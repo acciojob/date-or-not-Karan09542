54ff049c3188456b927ee8151fd52abdeda0648f
Karan vishwakarma <150724533+Karan09542@users.noreply.github.com>
2025-09-02 21:36:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 var isDate = function (input) {
   //   write your code here
-	if(new Date(input) != "Invalid Date") return true;
+	// if(new Date(input) != "Invalid Date") return true;
+	const date = new Date(input);
+	if(date instanceof Date && !isNaN(date.getTime())) return true
 	else return false
-	
 };
 
 // Do not change the code below.
-const input = prompt("Enter Date.");
-alert(isDate(input));
+// const input = prompt("Enter Date.");
+// alert(isDate(input));
 
